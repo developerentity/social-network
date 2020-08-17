b70ec7c171a4de15069e66b3eb83b692de273e16
just13just <bkryzhniy@gmail.com>
2020-08-17 20:30:59 +0300

    refactor: change img and css folder path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/App.css';
+import './components/css/App.css';
 import Header from "./components/Header/Header";
 import Footer from './components/Footer/Footer';
 import Profile from './components/Profile/Profile';
