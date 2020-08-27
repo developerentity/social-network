27ab9b4632f67635113c7fb36a594281084ba5c4
just13just <bkryzhniy@gmail.com>
2020-08-27 21:32:47 +0300

    refactor: add functionality

--- src/components/Shuffle/Shuffle.js
+++ src/components/Shuffle/Shuffle.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import style from './Shuffle.module.css';
+import React, { useState } from 'react';
 import Block1 from './Block1';
 import Block2 from './Block2';
 import Block3 from './Block3';
 import Block4 from './Block4';
 
-const blocks = [
-    <Block1 />,
-    <Block2 />,
-    <Block3 />,
-    <Block4 />
-]
-
 const Shuffle = () => {
+
+    const [blocks, setBlocks] = useState([
+        <Block1 />,
+        <Block2 />,
+        <Block3 />,
+        <Block4 />
+    ])
+    
+    const shuffle = (blockNumber) => {
+        
+        const arr = [...blocks]
+        const temporary = []
+        const permColor = []
+        const shuffledArr = []
+
+        for (let i = 0; i < arr.length; i++) {
+            if (i === blockNumber) {
+              permColor.push(arr[i])
+            }
+            if (i !== blockNumber) {
+              temporary.push(arr[i])
+            } 
+        }
+
+        temporary.sort(() => Math.random() - 0.5)
+
+        for (let i = 0; i < arr.length; i++) {
+            if (i !== blockNumber) {
+              shuffledArr[i] = temporary.pop()
+            } 
+            if (i === blockNumber) {
+              shuffledArr[i] = permColor[0]
+            }     
+        }
+
+        return setBlocks(shuffledArr)
+    }
+
+    const revert = () => {
+        return setBlocks([
+            <Block1 />,
+            <Block2 />,
+            <Block3 />,
+            <Block4 />
+        ])
+    }
+
     return (
-        <div className={style.shuffle}>
-            {blocks [0]}
-            {blocks [1]}
-            {blocks [2]}
-            {blocks [3]} 
-        </div>
+        <>
+            <div onClick={ () => shuffle(0) }>
+                {blocks[0]}
+            </div>
+            <div onClick={ () => shuffle(1) }>
+                {blocks[1]}
+            </div>
+            <div onClick={ () => shuffle(2) }>
+                {blocks[2]}
+            </div>
+            <div onClick={ () => revert() }>
+                {blocks[3]}
+            </div>  
+        </>
     )
 }
 
