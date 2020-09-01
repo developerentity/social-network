f94dabeab70d67b53b866b654cf8355074cfb905
just13just <bkryzhniy@gmail.com>
2020-09-01 20:38:05 +0300

    refator: add some data to MyPosts component

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,8 +3,18 @@ import style from './MyPosts.module.css';
 
 const MyPosts = () => {
     return (
-        <div className={style.myPosts}>
-            some text
+        <div className={style.wrap}>
+            <h4> Feed </h4>
+            <div>
+                <div className="containerTextarea">
+                    <textarea />
+                </div>
+                <button>Add news</button>
+            </div>
+            <div className={style.posts}>
+                {/* <Post message='hi, this is my first post' likesCount='11' />
+                <Post message='Yo, this is my second post' likesCount='1144' /> */}
+            </div>
         </div>
     )
 }
