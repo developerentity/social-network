f6fe0d65858adf25ec8119481858ae405d38c16e
just13just <bkryzhniy@gmail.com>
2020-09-01 18:08:49 +0300

    feat: add dialogs and messages components

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,20 +1,66 @@
 import React from 'react';
 import style from './Messages.module.css';
+import { NavLink } from 'react-router-dom';
+
+const DialogItem = (props) => {
+    return (
+        <div className={style.dialog}>
+            <NavLink to={`/messages/${props.id}`} activeClassName={style.act}>
+                {props.name}
+            </NavLink>
+        </div>
+    )
+}
+const Message = (props) => {
+    return (
+        <div className={style.message}>
+            {props.message}
+            <br />
+            <br />
+        </div>
+    )
+}
+
 
 const Messages = () => {
     return (
-        <>
-            <div className={style.left}>
-                <h2>
-                    Left side
-                </h2>
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+
+                <DialogItem
+                    name="Bohdan"
+                    id="1"
+                />
+                <DialogItem
+                    name="Vera"
+                    id="2"
+                />
+                <DialogItem
+                    name="Nadezhda"
+                    id="3"
+                />
+                <DialogItem
+                    name="Lyubov"
+                    id="4"
+                />
+
             </div>
-            <div className={style.right}>
-                <h2>
-                    Right side
-                </h2>
+
+
+            <div className={style.messages}>
+
+                <Message
+                    message="First message to Dev"
+                />
+                <Message
+                    message="Second random message"
+                />
+                <Message
+                    message="Third crazy message"
+                />
             </div>
-        </>
+
+        </div>
     )
 }
 
