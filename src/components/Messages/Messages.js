d0efa8578615cafede74b3ea5cbebaa79121e0ba
just13just <bkryzhniy@gmail.com>
2020-09-02 18:39:06 +0300

    fix: win error "Warning: Each child in a list should have a unique "key" prop"

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,23 +4,23 @@ import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 let dialogsData = [
-    {id: 1, name: "Bogdan"},
-    {id: 2, name: "Vera"},
-    {id: 3, name: "Nadezhda"},
-    {id: 4, name: "Lyubov"}
+    {key: 1, id: 1, name: "Bogdan"},
+    {key: 2, id: 2, name: "Vera"},
+    {key: 3, id: 3, name: "Nadezhda"},
+    {key: 4, id: 4, name: "Lyubov"}
 ]
 
 let messagesData = [
-    {id: 1, m: "First message to Dev"},
-    {id: 2, m: "Second random message"},
-    {id: 3, m: "Third crazy message"},
+    {key: 1, m: "First message to Dev"},
+    {key: 2, m: "Second random message"},
+    {key: 3, m: "Third crazy message"}
 ]
 
 let dialogsElements = dialogsData
-    .map(person => <DialogItem id={person.id} name={person.name} />)
+    .map(person => <DialogItem key={person.key} id={person.id} name={person.name} />)
 
 let messagesElements = messagesData
-    .map(message => <Message message={message.m} />)
+    .map(message => <Message key={message.key} message={message.m} />)
 
 const Messages = () => {
     return (
