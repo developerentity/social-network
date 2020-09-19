e771ca1a28630fb804aad3feca72be0501332d5c
just13just <bkryzhniy@gmail.com>
2020-09-19 17:18:46 +0300

    refactor: rename state.js to store.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './components/css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './data/state';
+import store from './data/store';
 
 let updateEntireTree = (state) => {
   
