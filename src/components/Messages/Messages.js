349c8613b9ba38e3d53cd762c69ebcd002c64167
Bohdan <bkryzhniy@gmail.com>
2020-09-28 11:09:39 +0300

    refactor: remove excess code

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -6,10 +6,10 @@ import Message from './Message/Message';
 const Messages = (props) => {
 
     let dialogsElements = props.dialogsData
-        .map(person => <DialogItem key={person.key} id={person.id} name={person.name} />)
+        .map(person => <DialogItem key={person.id} id={person.id} name={person.name} />)
 
     let messagesElements = props.messagesData
-        .map(message => <Message key={message.key} message={message.m} />)
+        .map(message => <Message key={message.m.toString()} message={message.m} />)
 
     return (
         <div className={style.dialogs}>
--- src/data/store.js
+++ src/data/store.js
@@ -8,15 +8,15 @@ let store = {
     _state: {
         messagePage: {
             dialogsData: [
-                { key: 1, id: 1, name: "Bogdan" },
-                { key: 2, id: 2, name: "Vera" },
-                { key: 3, id: 3, name: "Nadezhda" },
-                { key: 4, id: 4, name: "Lyubov" }
+                { id: 1, name: "Bogdan" },
+                { id: 2, name: "Vera" },
+                { id: 3, name: "Nadezhda" },
+                { id: 4, name: "Lyubov" }
             ],
             messagesData: [
-                { key: 1, m: "First message to Dev" },
-                { key: 2, m: "Second random message" },
-                { key: 3, m: "Third crazy message" }
+                { m: "First message to Dev" },
+                { m: "Second random message" },
+                { m: "Third crazy message" }
             ]
         },
         profilePage: {
