5ceaf2cd28200d873cff2bf30978fed57617aa6d
Bohdan <bkryzhniy@gmail.com>
2020-11-07 19:12:58 +0200

    feat: integrate redux-form lib to Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component={'input'} />
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'} />
+            </div>
+            <div>
+                <Field type={'Checkbox'} name={'checkbox'} component={'input'} /> Remember me
+               </div>
+            <div>
+                <button type={'submit'} >submit</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
 
 const Login = () => {
+
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
     return (
-        <h1>Login page</h1>
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
     )
 }
 
