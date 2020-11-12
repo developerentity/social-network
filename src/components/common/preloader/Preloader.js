58bd924ca5263f90bffe67ff1057db509b0fae57
Bohdan <bkryzhniy@gmail.com>
2020-11-12 22:11:15 +0200

    fix: uncaught glich while switching users page number

--- src/components/common/preloader/Preloader.js
+++ src/components/common/preloader/Preloader.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Preloader.module.css';
-import loading from '../../../assets/images/loading.gif';
+// import loading from '../../../assets/images/loading.gif';
 
 const Preloader = () => {
     return (
         <div className={style.preloaderWrap}>
-            <img src={loading} alt='...' />
+            {/* <img src={loading} alt='...' /> */}
+            <h1>Preloader</h1>
         </div>
     )
 }
