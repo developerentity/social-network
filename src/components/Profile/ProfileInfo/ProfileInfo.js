dba57132e9816cc16ed19e871239b4c686bf7d63
Bohdan <bkryzhniy@gmail.com>
2020-11-14 20:04:26 +0200

    style: ProfileInfo component

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './ProfileInfo.module.css';
-// import wallpaper from './../../img/wall.jpg'
+import wallpaper from './../../img/wall.jpg'
 import Preloader from '../../common/preloader/Preloader';
 import ProfileStatus from './ProfileStatus';
 
@@ -19,26 +19,31 @@ const ProfileInfo = (props) => {
     }
     return (
         <>
-            {/* <div className={style.wallpaper}>
-                <img src={wallpaper} alt="..." />
-            </div> */}
+            <div
+                className={style.wallpaper}
+                style={{
+                    backgroundImage: `url(${profile.photos.large || wallpaper})`
+                }}>
+                <div className={style.infoWrap}>
+                    <div className={style.wrap}>
+                        <div className={style.contentWrap}>
+                            <div>
+                                <img src={profile.photos.small || 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png'} alt="..." />
+                            </div>
+                            <div><ProfileStatus
+                                userStatus={userStatus}
+                                getUpdateStatus={getUpdateStatus}
+                            /></div>
 
-            <div className={style.wrap}>
-                <div className={style.avatar}>
-                    <img src={profile.photos.large} alt="..." />
+                            <div>
+                                <h3>{profile.fullName}</h3>
+                                {profile.aboutMe ? <p>About me: {profile.aboutMe}</p> : null}
+                                {profile.contacts.facebook ? <p>Fb: {profile.contacts.facebook}</p> : null}
+                                {profile.contacts.instagram ? <p>Inst: {profile.contacts.instagram}</p> : null}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <ProfileStatus
-                    userStatus={userStatus}
-                    getUpdateStatus={getUpdateStatus}
-                />
-
-                <div>
-                    <h3>{profile.fullName}</h3>
-                    {profile.aboutMe ? <p>About me: {profile.aboutMe}</p> : null}
-                    {profile.contacts.facebook ? <p>Fb: {profile.contacts.facebook}</p> : null}
-                    {profile.contacts.instagram ? <p>Inst: {profile.contacts.instagram}</p> : null}
-                </div>
-
             </div>
         </>
     )
