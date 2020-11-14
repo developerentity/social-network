2aa2603e7756ea87a2ce655af29cd144129b84ef
Bohdan <bkryzhniy@gmail.com>
2020-11-14 17:59:03 +0200

    style: header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './Header.module.css';
 
 const Header = (props) => {
 
+    const [hide, setHide] = useState(false)
+
     return (
         <div className={style.header}>
-            <h1>Header text</h1>
+            <div className={style.headerText}>The orbit</div>
             <div className={style.loginNL}>
-                {props.isAuth
-                    ? <div>{props.login} <button onClick={props.getLogout}>logout</button></div>
-                    : <NavLink to='/login'>Login</NavLink>}
+                <div className={style.loginInput}>
+                    {props.isAuth
+                        ? <div className={style.btn}
+                            onMouseOver={() => setHide(true)}
+                            onMouseLeave={() => setHide(false)} >
+                            <div className={hide ? style.hide : null}>{props.login}</div>
+                            <div className={!hide ? style.hide : null} onClick={props.getLogout}>Logout</div>
+                        </div>
+                        : <NavLink to='/login'><div className={style.btn + ' ' + style.login}>Login</div> </NavLink>}
+                </div>
             </div>
         </div>
     )
