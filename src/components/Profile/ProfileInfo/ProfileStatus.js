001aa62d7ba67abafa181f6b57abb5fd1d3a526b
Bohdan <bkryzhniy@gmail.com>
2020-11-16 23:30:06 +0200

    fix: solve problem with changing authorized profile status while trying change another profile status (using useSelect)

--- src/components/Profile/ProfileInfo/ProfileStatus.js
+++ src/components/Profile/ProfileInfo/ProfileStatus.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const ProfileStatus = (props) => {
 
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.userStatus);
 
+    const authorizedUserId = useSelector(state => state.auth.userId)
+    const selectedProfileUserId = useSelector(state => state.profilePage.profile.userId)
+
     const activateEditMode = () => {
-        setEditMode(true)
+        if (authorizedUserId === selectedProfileUserId) setEditMode(true)
     }
     const deactivateEditMode = () => {
         setEditMode(false)
