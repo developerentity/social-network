f81c0cf562cee62cabe3345265990ae9e6b2548f
Bohdan <bkryzhniy@gmail.com>
2020-11-17 00:39:40 +0200

    refactor: App to functional component (using useDispatch, useSelect)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './components/css/App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Footer from './components/Footer/Footer';
@@ -16,51 +16,48 @@ import {
 import ProfileContainer from './components/Profile/ProfileContainer';
 import Login from './components/Login/Login';
 import { getInitializeApp } from './redux/appReducer';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Preloader from './components/common/preloader/Preloader';
 
-class App extends React.Component {
+const App = () => {
 
-  componentDidMount() {
-    this.props.getInitializeApp()
+  const initialized = useSelector(state => state.app.initialized)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getInitializeApp())
+  }, [dispatch])
+
+  if (!initialized) {
+    return <Preloader />
   }
-
-  render() {
-    if (!this.props.initialized) {
-      return <Preloader />
-    }
-    return (
-      <div className="container">
-        <div className="grid">
-          <Router>
-            <HeaderContainer />
-            <Nav />
-            <div className='content'>
-              <Route path='/profile/:userId?'>
-                <ProfileContainer />
-              </Route>
-              <Route path='/messages'>
-                <MessagesContainer />
-              </Route>
-              <Route path='/users'>
-                <UsersContainer />
-              </Route>
-              <Route path='/news' component={News} />
-              <Route path='/music' component={Music} />
-              <Route path='/settings' component={Settings} />
-              <Route path='/shuffle' component={Shuffle} />
-              <Route path='/login' component={Login} />
-            </div>
-            <Footer />
-          </Router>
-        </div>
+  return (
+    <div className="container">
+      <div className="grid">
+        <Router>
+          <HeaderContainer />
+          <Nav />
+          <div className='content'>
+            <Route path='/profile/:userId?'>
+              <ProfileContainer />
+            </Route>
+            <Route path='/messages'>
+              <MessagesContainer />
+            </Route>
+            <Route path='/users'>
+              <UsersContainer />
+            </Route>
+            <Route path='/news' component={News} />
+            <Route path='/music' component={Music} />
+            <Route path='/settings' component={Settings} />
+            <Route path='/shuffle' component={Shuffle} />
+            <Route path='/login' component={Login} />
+          </div>
+          <Footer />
+        </Router>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  initialized: state.app.initialized
-})
-
-export default connect(mapStateToProps, { getInitializeApp })(App)
+export default App;
