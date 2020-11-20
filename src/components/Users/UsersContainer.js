976487c2bc179ba1f1abf5a3f2c4e818edf7da26
Bohdan <bkryzhniy@gmail.com>
2020-11-20 15:29:16 +0200

    refactor: translate UsersContainer from class to functional component

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     follow,
     unfollow,
@@ -9,7 +9,6 @@ import {
 import Users from './Users';
 import Preloader from '../common/preloader/Preloader';
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
-import { compose } from 'redux';
 import {
     getUsersSelector,
     getPageSizeSelector,
@@ -19,59 +18,40 @@ import {
     getIsFollowingProgressSelector
 } from '../../redux/usersSelector';
 
-class UsersContainer extends React.Component {
+const UsersContainer = () => {
 
-    componentDidMount() {
-        this.props.getUsers(this.props.currentPage, this.props.pageSize)
+    const dispatch = useDispatch()
+    const users = useSelector(state => getUsersSelector(state))
+    const pageSize = useSelector(state => getPageSizeSelector(state))
+    const totalUsersCount = useSelector(state => getTotalUsersCountSelector(state))
+    const currentPage = useSelector(state => getCurrentPageSelector(state))
+    const isFetching = useSelector(state => getIsFetchingSelector(state))
+    const isFollowingProgress = useSelector(state => getIsFollowingProgressSelector(state))
+
+    useEffect(() => {
+        dispatch(getUsers(currentPage, pageSize))
+        return () => dispatch(setCurrentPage(1))
+    }, [dispatch, currentPage, pageSize])
+
+    const onPageChanged = (pageNumber) => {
+        dispatch(getUsers(pageNumber, pageSize))
     }
 
-    componentWillUnmount() {
-        this.props.setCurrentPage(1)
-    }
-
-    onPageChanged = (pageNumber) => {
-        this.props.getUsers(pageNumber, this.props.pageSize)
-    }
-
-    render() {
-        return (
-            <>{this.props.isFetching
-                ? <Preloader />
-                : <Users
-                    totalUsersCount={this.props.totalUsersCount}
-                    pageSize={this.props.pageSize}
-                    currentPage={this.props.currentPage}
-                    users={this.props.users}
-                    follow={this.props.follow}
-                    unfollow={this.props.unfollow}
-                    onPageChanged={this.onPageChanged}
-                    isFollowingProgress={this.props.isFollowingProgress}
-                />}
-            </>
-        )
-    }
-}
-
-const mapStateToProps = (state) => {
-    return {
-        users: getUsersSelector(state),
-        pageSize: getPageSizeSelector(state),
-        totalUsersCount: getTotalUsersCountSelector(state),
-        currentPage: getCurrentPageSelector(state),
-        isFetching: getIsFetchingSelector(state),
-        isFollowingProgress: getIsFollowingProgressSelector(state)
-    }
-}
-
-export default compose(
-    withAuthRedirect,
-    connect(
-        mapStateToProps,
-        {
-            follow,
-            unfollow,
-            getUsers,
-            setCurrentPage
-        }
+    return (
+        <>{isFetching
+            ? <Preloader />
+            : <Users
+                totalUsersCount={totalUsersCount}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                users={users}
+                follow={dispatch(follow)}
+                unfollow={dispatch(unfollow)}
+                onPageChanged={onPageChanged}
+                isFollowingProgress={isFollowingProgress}
+            />}
+        </>
     )
-)(UsersContainer);
+}
+
+export default withAuthRedirect(UsersContainer);
