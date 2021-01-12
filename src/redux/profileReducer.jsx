7700f159eac64a14935ca4f3ca0fa35964491fd2
just13just <bkryzhniy@gmail.com>
2021-01-12 15:07:56 +0200

    fix: not found img

--- src/redux/profileReducer.jsx
+++ src/redux/profileReducer.jsx
@@ -22,27 +22,27 @@ const savePhotoSuccess = (photos) => {
 const initialState = {
     postsData: [
         {
-            avatar: `https://coubsecure-s.akamaihd.net/get/b81/p/coub/simple/cw_timeline_pic/691c3a60c6a/d02de237a879bceafbd0e/med_1542127707_image.jpg`,
+            avatar: `https://cdn.shazoo.ru/484375_17F3XlRozj_484361_hyk7rvse2u_bf3cdf05_838c.jpg`,
             text: `First post text`,
             like: 13
         },
         {
-            avatar: `https://coubsecure-s.akamaihd.net/get/b81/p/coub/simple/cw_timeline_pic/691c3a60c6a/d02de237a879bceafbd0e/med_1542127707_image.jpg`,
+            avatar: `https://cdn.shazoo.ru/484375_17F3XlRozj_484361_hyk7rvse2u_bf3cdf05_838c.jpg`,
             text: `Second post text`,
             like: 21
         },
         {
-            avatar: `https://coubsecure-s.akamaihd.net/get/b81/p/coub/simple/cw_timeline_pic/691c3a60c6a/d02de237a879bceafbd0e/med_1542127707_image.jpg`,
+            avatar: `https://cdn.shazoo.ru/484375_17F3XlRozj_484361_hyk7rvse2u_bf3cdf05_838c.jpg`,
             text: `Third post text`,
             like: 8
         },
         {
-            avatar: `https://coubsecure-s.akamaihd.net/get/b81/p/coub/simple/cw_timeline_pic/691c3a60c6a/d02de237a879bceafbd0e/med_1542127707_image.jpg`,
+            avatar: `https://cdn.shazoo.ru/484375_17F3XlRozj_484361_hyk7rvse2u_bf3cdf05_838c.jpg`,
             text: `Fourth post text`,
             like: 5
         },
         {
-            avatar: `https://coubsecure-s.akamaihd.net/get/b81/p/coub/simple/cw_timeline_pic/691c3a60c6a/d02de237a879bceafbd0e/med_1542127707_image.jpg`,
+            avatar: `https://cdn.shazoo.ru/484375_17F3XlRozj_484361_hyk7rvse2u_bf3cdf05_838c.jpg`,
             text: `Fifth post text`,
             like: 21
         }
@@ -56,7 +56,7 @@ const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST: {
             let newPost = {
-                avatar: `https://coubsecure-s.akamaihd.net/get/b81/p/coub/simple/cw_timeline_pic/691c3a60c6a/d02de237a879bceafbd0e/med_1542127707_image.jpg`,
+                avatar: `https://cdn.shazoo.ru/484375_17F3XlRozj_484361_hyk7rvse2u_bf3cdf05_838c.jpg`,
                 text: action.body,
                 like: 0
             }
